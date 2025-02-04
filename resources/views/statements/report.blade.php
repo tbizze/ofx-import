f877338302decc1dbdff31ee461a766528c8362d
Tiago Bizze <Tiago Bizze>
2025-02-04 12:27:16 -0300

    OFX-09: criado interface de envio dos arquivos

---
+++ resources/views/statements/report.blade.php
@@ -0,0 +1,58 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Transações') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class=" text-gray-900 dark:text-gray-100">
+
+                    {{-- Chama compomente para exibir flesh message --}}
+                    {{-- <x-flash-message /> --}}
+
+                    <!-- Aqui o conteúdo -->
+                    <div class="p-5">
+                        @if ($errors->any())
+                            @foreach ($errors->all() as $error)
+                                <div class=" text-red-500">{{ $error }}</div>
+                            @endforeach
+                        @endif
+                    </div>
+
+                    <x-my.form action="{{ route('process.upload') }}" method="POST" enctype="multipart/form-data"
+                        class="">
+
+                        <x-my.input-drag-drop name="transactions_file" label="TRANSAÇÕES: Escolha arquivo..." />
+
+                        <div class="mt-10">
+                            <x-my.input-drag-drop name="statements_file" label="EXTRATO: Escolha arquivo..." />
+                        </div>
+
+                        <x-slot:actions>
+                            <x-my.button type="submit">Salvar</x-my.button>
+                            <x-my.button link style="secondary"
+                                href="{{ route('bank-accounts.index') }}">Cancelar</x-my.button>
+                        </x-slot:actions>
+                    </x-my.form>
+
+
+                    @if (isset($result))
+                        <h2>Resultados</h2>
+                        <h3>Correspondências:</h3>
+                        <pre>{{ print_r($result['matches'], true) }}</pre>
+                        <h3>Transações Não Encontradas:</h3>
+                        <pre>{{ print_r($result['unmatched_transactions'], true) }}</pre>
+                        <h3>Lançamentos Não Correspondentes:</h3>
+                        <pre>{{ print_r($result['unmatched_statements'], true) }}</pre>
+                    @endif
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
